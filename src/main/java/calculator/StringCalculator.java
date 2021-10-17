59cf23619ce70aef055e81beff10bf1099f894fe
hingawepallavi86 <92634778+hingawepallavi86@users.noreply.github.com>
2021-10-17 13:53:44 +0530

    Green test: to return sum for two numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,8 +3,13 @@ package calculator;
 class StringCalculator {
 
     public int add(String numbers) {
+    	String[]input=numbers.split(",");
     	if(isEmpty(numbers))
         return 0;
+    	else if(input.length>1) {
+    		return Integer.parseInt(input[0])+Integer.parseInt(input[1]);
+    	
+    	}
     	return stringToInt(numbers);
     }
     public boolean isEmpty(String numbers) {
