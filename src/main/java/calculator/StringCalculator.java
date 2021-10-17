e40bc8a5b014f075e65e41dd4f93c4cafdbd8bc2
hingawepallavi86 <92634778+hingawepallavi86@users.noreply.github.com>
2021-10-17 13:42:02 +0530

    Refractor: to return signle number

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,6 +5,9 @@ class StringCalculator {
     public int add(String input) {
     	if(input.isEmpty())
         return 0;
+    	return stringToInt(input);
+    }
+    public int stringToInt(String input) {
     	return Integer.parseInt(input);
     }
 
